89a15295163c6ea10b4591b3d69213a35e128048
Chilli Chelli <ramin@sushi.com>
2023-12-30 03:23:14 +0000

    add header

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { DropZone } from "@/components/drop-zone";
 import { Main } from "@/components/ui/main";
-import { Separator } from "@/components/ui/separator";
 import { WalletsList } from "@/components/wallets-list";
 import { fontSans } from "@/lib/fonts";
 import { cn } from "@/lib/utils";
@@ -11,20 +10,23 @@ export default function IndexPage() {
     <Main>
       <div className="flex flex-col gap-6 items-baseline">
         <div className="flex flex-col mb-4">
-          <h1
-            className={cn(
-              fontSans.className,
-              "flex gap-6 items-center text-[40px] tracking-tighter font-bold",
-            )}
-          >
-            Wallet Finder
-          </h1>
           <a
             href="https://twitter.com/chillichelli"
             className="text-blue-500 font-bold text-lg"
           >
             @chillichelli
           </a>
+          <h1
+            className={cn(
+              fontSans.className,
+              "flex gap-6 items-center text-[40px] tracking-tighter font-bold",
+            )}
+          >
+            Wallet Finder{" "}
+          </h1>
+          <span className="-mt-1 text-muted-foreground font-medium pl-1">
+            for the kennel
+          </span>
         </div>
         <div className="flex lg:flex-row flex-col gap-6 w-full">
           <div>
